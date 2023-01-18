7238050675fd4b1161375ef60357aaab84e49b4d
Anil Kumar <anil.kumar@habilelabs.io>
2023-01-18 16:45:28 +0530

    jquery task files uploaded

---
+++ js/jqeury2.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  // 2. Count child elements using jQuery.
+  $("#childs").click(function () {
+    // console.log($("#selected").children().length);
+    console.log("Length of Childs:", $("#selected p").length);
+  });
+
+  //   3. Find the total width of an element:-
+  let tagLen = $("#myText").width();
+  console.log(`width of the P Tag: ${tagLen}`);
+
+  //   4. Check if an object is a jQuery object
+  var test = $("#selected");
+  if (test instanceof jQuery) {
+    console.log("object is jQuery");
+  }
+  $("#button1").click(function () {
+    $("#pid").toggleClass("large");
+  });
+});
---
+++ js/jquery.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $("#classRemover").click(function () {
+    $("*").removeClass();
+  });
+});
